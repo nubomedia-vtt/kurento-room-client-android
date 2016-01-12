ff88d7c448f8232c2e508e3d410ebf16aebc138b
Ahola Jukka <jukka.ahola@vtt.fi>
2016-01-12 09:08:50 +0200

    1.0.2 notification static fields added

--- kurento-room-client-android/src/main/java/fi/vtt/nubomedia/kurentoroomclientandroid/RoomListener.java
+++ kurento-room-client-android/src/main/java/fi/vtt/nubomedia/kurentoroomclientandroid/RoomListener.java
@@ -1,10 +1,24 @@
 package fi.vtt.nubomedia.kurentoroomclientandroid;
 
 public interface RoomListener {
+
+    /**
+     *  Notification method names
+     */
+    public static final String METHOD_PARTICIPANT_JOINED = "participantJoined";
+    public static final String METHOD_PARTICIPANT_PUBLISHED = "participantPublished";
+    public static final String METHOD_PARTICIPANT_UNPUBLISHED = "participantUnpublished";
+    public static final String METHOD_ICE_CANDIDATE = "iceCandidate";
+    public static final String METHOD_PARTICIPANT_LEFT = "participantLeft";
+    public static final String METHOD_SEND_MESSAGE = "sendMessage";
+    public static final String METHOD_MEDIA_ERROR = "mediaError";
+
+
     public void onRoomResponse(RoomResponse response);
 
     public void onRoomError(RoomError error);
 
     public void onRoomNotification(RoomNotification notification);
+
 
 }
