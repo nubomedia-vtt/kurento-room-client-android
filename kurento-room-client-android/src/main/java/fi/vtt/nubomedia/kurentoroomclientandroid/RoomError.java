c1532bef657383ad3e6ba10faccedefd1f581084
Janne Seppänen <janne.seppanen@vtt.fi>
2016-10-07 14:16:39 +0300

    Error code is now integer instead of int and error enum types are copied to RoomError class. This is a workaround to avoid dependency to kurento server but still to allow users to compare error codes to enums

--- kurento-room-client-android/src/main/java/fi/vtt/nubomedia/kurentoroomclientandroid/RoomError.java
+++ kurento-room-client-android/src/main/java/fi/vtt/nubomedia/kurentoroomclientandroid/RoomError.java
@@ -6,20 +6,49 @@ import fi.vtt.nubomedia.jsonrpcwsandroid.JsonRpcResponseError;
  * Room error class
  */
 public class RoomError {
-    private String code = null;
+
+    /* Copied from https://github.com/Kurento/kurento-room/blob/master/kurento-room-sdk/src/main/java/org/kurento/room/exception/RoomException.java
+       to avoid dependency to server for error checking
+    */
+    public static enum Code {
+        GENERIC_ERROR_CODE(999),
+
+        TRANSPORT_ERROR_CODE(803), TRANSPORT_RESPONSE_ERROR_CODE(802), TRANSPORT_REQUEST_ERROR_CODE(801),
+
+        MEDIA_MUTE_ERROR_CODE(307), MEDIA_NOT_A_WEB_ENDPOINT_ERROR_CODE(306), MEDIA_RTP_ENDPOINT_ERROR_CODE(
+                305), MEDIA_WEBRTC_ENDPOINT_ERROR_CODE(304), MEDIA_ENDPOINT_ERROR_CODE(303), MEDIA_SDP_ERROR_CODE(
+                302), MEDIA_GENERIC_ERROR_CODE(301),
+
+        ROOM_CANNOT_BE_CREATED_ERROR_CODE(204), ROOM_CLOSED_ERROR_CODE(203), ROOM_NOT_FOUND_ERROR_CODE(
+                202), ROOM_GENERIC_ERROR_CODE(201),
+
+        USER_NOT_STREAMING_ERROR_CODE(105), EXISTING_USER_IN_ROOM_ERROR_CODE(104), USER_CLOSED_ERROR_CODE(
+                103), USER_NOT_FOUND_ERROR_CODE(102), USER_GENERIC_ERROR_CODE(101);
+        private int value;
+
+        Code(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+    }
+
+    private int code = 0;
     private String data = null;
 
     public RoomError(JsonRpcResponseError error){
         super();
         if(error!=null) {
-            this.code = "" + error.getCode();
+            this.code = error.getCode();
             if(error.getData()!=null) {
                 this.data = error.getData().toString();
             }
         }
     }
 
-    public String getCode() {
+    public int getCode() {
         return code;
     }
 
